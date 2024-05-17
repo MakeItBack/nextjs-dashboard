bb2cfa89f70e3601d7ec65dfb3486d369aec8f12
Mike Back <hello@mikeback.me>
2024-05-17 16:19:30 +1000

    feat: streaming components and loading.tsx pages for dynamic content

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,38 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+
+// Refresh the dashboard page, you should see the main dashboard information almost immediately,
+// BUT a fallback skeleton is shown for <RevenueChart> and <LatestInvoices> which is wrapped in a <Suspense> to load it separately when available
+// This is replicating what you would do if there was one element that was slow to load and blocking the page load.
+// This way the page loads and is not blocked.
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
