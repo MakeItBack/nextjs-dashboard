2707133ac6555f391dd58e777be664cb384c6c9e
Mike Back <hello@mikeback.me>
2024-05-21 21:45:00 +1000

    feat: rebrand!

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { BeakerIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,10 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <div>
+        <BeakerIcon className="mr-8 h-14 w-14 rotate-[15deg]" />
+      </div>
+      <p className="text-[44px]">Cat Lab</p>
     </div>
   );
 }
