732b99a5a9bcc8d360656f9734542e420618c0c5
Mike Back <hello@mikeback.me>
2024-05-21 22:13:07 +1000

    feat: edit and delete invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,7 +28,9 @@ const FormSchema = zod.object({
 /* The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
 You can then pass your rawFormData to CreateInvoice to validate the types: */
 
+// Use Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -50,7 +52,31 @@ to eliminate JavaScript floating-point errors: */
   /* Next.js has a Client-side Router Cache that stores the route segments in the user's browser for a time.
 Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of server requests.
 As we're updating the data displayed in the invoices route, we need to clear this cache and trigger a new request to the server.
-You can do this with the revalidatePath function */
+You can do this with the revalidatePath function. */
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
